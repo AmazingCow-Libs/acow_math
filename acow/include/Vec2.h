264255a101800c12c52adb4615d6b8286df2bd3c
n2omatt <n2omatt@amazingcow.com>
2018-02-25 20:35:41 -0300

    - Add the acow_goodies support for constexpr.

--- acow/include/Vec2.h
+++ acow/include/Vec2.h
@@ -2,6 +2,8 @@
 
 // std
 #include <cmath>
+// AmazingCow Libs
+#include "acow/cpp_goodies.h"
 // acow_math_goodies
 #include "Constants.h"
 
@@ -13,29 +15,32 @@ struct Vec2
     //------------------------------------------------------------------------//
     // Static Functions                                                       //
     //------------------------------------------------------------------------//
-    constexpr inline static Vec2 Zero() noexcept { return Vec2{ 0.0f, 0.0f }; }
-    constexpr inline static Vec2 One () noexcept { return Vec2{ 1.0f, 1.0f }; }
-    constexpr inline static Vec2 Half() noexcept { return Vec2{ 0.5f, 0.5f }; }
+public:
+    ACOW_CONSTEXPR_STRICT inline static Vec2 Zero() noexcept { return Vec2{ 0.0f, 0.0f }; }
+    ACOW_CONSTEXPR_STRICT inline static Vec2 One () noexcept { return Vec2{ 1.0f, 1.0f }; }
+    ACOW_CONSTEXPR_STRICT inline static Vec2 Half() noexcept { return Vec2{ 0.5f, 0.5f }; }
 
-    constexpr inline static Vec2 Left () noexcept { return Vec2{ -1.0f,  0.0f }; }
-    constexpr inline static Vec2 Right() noexcept { return Vec2{ +1.0f,  0.0f }; }
-    constexpr inline static Vec2 Up   () noexcept { return Vec2{  0.0f, -1.0f }; }
-    constexpr inline static Vec2 Down () noexcept { return Vec2{  0.0f, +1.0f }; }
+    ACOW_CONSTEXPR_STRICT inline static Vec2 Left () noexcept { return Vec2{ -1.0f,  0.0f }; }
+    ACOW_CONSTEXPR_STRICT inline static Vec2 Right() noexcept { return Vec2{ +1.0f,  0.0f }; }
+    ACOW_CONSTEXPR_STRICT inline static Vec2 Up   () noexcept { return Vec2{  0.0f, -1.0f }; }
+    ACOW_CONSTEXPR_STRICT inline static Vec2 Down () noexcept { return Vec2{  0.0f, +1.0f }; }
 
 
     //-------------------------------------------------------------------------//
     // Public Vars                                                             //
     //-------------------------------------------------------------------------//
+public:
     float x; float y;
 
 
     //------------------------------------------------------------------------//
     // CTOR / DTOR                                                            //
     //------------------------------------------------------------------------//
-    constexpr explicit Vec2(float x = 0.0f, float y = 0.0f) noexcept :
-        x(x), y(y)
+public:
+    ACOW_CONSTEXPR_STRICT explicit Vec2(float x = 0.0f, float y = 0.0f) noexcept
+        : x(x), y(y)
     {
-        //Empty...
+        // Empty...
     }
 
 
@@ -43,19 +48,19 @@ struct Vec2
     //------------------------------------------------------------------------//
     // Magnitude                                                              //
     //------------------------------------------------------------------------//
-    inline float Magnitude   () { return sqrtf(x*x + y*y); }
-    inline float MagnitudeSqr() { return       x*x + y*y;  }
+    ACOW_CONSTEXPR_STRICT float Magnitude   () { return sqrtf(x*x + y*y); }
+    ACOW_CONSTEXPR_STRICT float MagnitudeSqr() { return       x*x + y*y;  }
 
 
     //------------------------------------------------------------------------//
     // Distance                                                               //
     //------------------------------------------------------------------------//
-    inline float Distance(const Vec2 v2)
+    ACOW_CONSTEXPR_STRICT inline float Distance(const Vec2 v2)
     {
         return sqrtf(powf(x - v2.x, 2) + powf(y - v2.y, 2));
     }
 
-    inline float DistanceSqr(const Vec2 v2)
+    ACOW_CONSTEXPR_STRICT inline float DistanceSqr(const Vec2 v2)
     {
         return powf(x - v2.x, 2) + powf(y - v2.y, 2);
     }
@@ -64,13 +69,13 @@ struct Vec2
     //------------------------------------------------------------------------//
     // Normalize                                                              //
     //------------------------------------------------------------------------//
-    inline void Normalize()
+    ACOW_CONSTEXPR_LOOSE inline void Normalize()
     {
         auto magnitude = Magnitude();
         x /= magnitude; y /= magnitude;
     }
 
-    inline Vec2 Normalized()
+    ACOW_CONSTEXPR_LOOSE inline Vec2 Normalized()
     {
         auto vec2 = Vec2(*this);
         vec2.Normalize();
@@ -82,7 +87,7 @@ struct Vec2
     //------------------------------------------------------------------------//
     // Rotation                                                               //
     //------------------------------------------------------------------------//
-    inline void Rotate(float degrees)
+    ACOW_CONSTEXPR_LOOSE inline void Rotate(float degrees)
     {
         auto r = (degrees * math::kDegrees2Radians);
         auto s = sinf(r);
@@ -92,7 +97,7 @@ struct Vec2
         y = (x * s + y * c);
     }
 
-    inline Vec2 Rotated(float degrees) const
+    ACOW_CONSTEXPR_LOOSE inline Vec2 Rotated(float degrees) const
     {
         auto vec2 = Vec2(*this);
         vec2.Rotate(degrees);
@@ -104,19 +109,19 @@ struct Vec2
     //------------------------------------------------------------------------//
     // Operators                                                              //
     //------------------------------------------------------------------------//
-    constexpr friend Vec2 operator +(const Vec2 &lhs, const Vec2 &rhs);
-    constexpr friend Vec2 operator -(const Vec2 &lhs, const Vec2 &rhs);
-    constexpr friend Vec2 operator *(const Vec2 &lhs, const Vec2 &rhs);
-    constexpr friend Vec2 operator /(const Vec2 &lhs, const Vec2 &rhs);
+    ACOW_CONSTEXPR_STRICT friend Vec2 operator +(const Vec2 &lhs, const Vec2 &rhs);
+    ACOW_CONSTEXPR_STRICT friend Vec2 operator -(const Vec2 &lhs, const Vec2 &rhs);
+    ACOW_CONSTEXPR_STRICT friend Vec2 operator *(const Vec2 &lhs, const Vec2 &rhs);
+    ACOW_CONSTEXPR_STRICT friend Vec2 operator /(const Vec2 &lhs, const Vec2 &rhs);
 
-    constexpr friend Vec2& operator +=(Vec2 &lhs, const Vec2 &rhs);
-    constexpr friend Vec2& operator -=(Vec2 &lhs, const Vec2 &rhs);
-    constexpr friend Vec2& operator *=(Vec2 &lhs, const Vec2 &rhs);
-    constexpr friend Vec2& operator /=(Vec2 &lhs, const Vec2 &rhs);
+    ACOW_CONSTEXPR_LOOSE  friend Vec2& operator +=(Vec2 &lhs, const Vec2 &rhs);
+    ACOW_CONSTEXPR_LOOSE  friend Vec2& operator -=(Vec2 &lhs, const Vec2 &rhs);
+    ACOW_CONSTEXPR_LOOSE  friend Vec2& operator *=(Vec2 &lhs, const Vec2 &rhs);
+    ACOW_CONSTEXPR_LOOSE  friend Vec2& operator /=(Vec2 &lhs, const Vec2 &rhs);
 
 
-    constexpr friend Vec2 operator*(const Vec2 &lhs, float scalar);
-    constexpr friend Vec2 operator*(float scalar, const Vec2 &rhs);
+    ACOW_CONSTEXPR_STRICT friend Vec2 operator*(const Vec2 &lhs, float scalar);
+    ACOW_CONSTEXPR_STRICT friend Vec2 operator*(float scalar, const Vec2 &rhs);
 
 }; //struct Vec2
 
@@ -124,62 +129,61 @@ struct Vec2
 //----------------------------------------------------------------------------//
 // Operators Implementation.                                                  //
 //----------------------------------------------------------------------------//
-constexpr inline Vec2 operator +(const Vec2 &lhs, const Vec2 &rhs)
+ACOW_CONSTEXPR_STRICT inline Vec2 operator +(const Vec2 &lhs, const Vec2 &rhs)
 {
     return Vec2(lhs.x + rhs.x, lhs.y + rhs.y);
 }
 
-constexpr inline Vec2 operator -(const Vec2 &lhs, const Vec2 &rhs)
+ACOW_CONSTEXPR_STRICT inline Vec2 operator -(const Vec2 &lhs, const Vec2 &rhs)
 {
     return Vec2(lhs.x - rhs.x, lhs.y - rhs.y);
 }
 
-constexpr inline Vec2 operator *(const Vec2 &lhs, const Vec2 &rhs)
+ACOW_CONSTEXPR_STRICT inline Vec2 operator *(const Vec2 &lhs, const Vec2 &rhs)
 {
     return Vec2(lhs.x * rhs.x, lhs.y * rhs.y);
 }
 
-constexpr inline Vec2 operator /(const Vec2 &lhs, const Vec2 &rhs)
+ACOW_CONSTEXPR_STRICT inline Vec2 operator /(const Vec2 &lhs, const Vec2 &rhs)
 {
     return Vec2(lhs.x / rhs.x, lhs.y / rhs.y);
 }
 
 
-constexpr inline Vec2& operator +=(Vec2 &lhs, const Vec2 &rhs)
+ACOW_CONSTEXPR_LOOSE inline Vec2& operator +=(Vec2 &lhs, const Vec2 &rhs)
 {
     lhs.x += rhs.x; lhs.y += rhs.y;
     return lhs;
 }
 
-constexpr inline Vec2& operator -=(Vec2 &lhs, const Vec2 &rhs)
+ACOW_CONSTEXPR_LOOSE inline Vec2& operator -=(Vec2 &lhs, const Vec2 &rhs)
 {
     lhs.x -= rhs.x; lhs.y -= rhs.y;
     return lhs;
 }
 
-constexpr inline Vec2& operator *=(Vec2 &lhs, const Vec2 &rhs)
+ACOW_CONSTEXPR_LOOSE inline Vec2& operator *=(Vec2 &lhs, const Vec2 &rhs)
 {
     lhs.x *= rhs.x; lhs.y *= rhs.y;
     return lhs;
 }
 
-constexpr inline Vec2& operator /=(Vec2 &lhs, const Vec2 &rhs)
+ACOW_CONSTEXPR_LOOSE inline Vec2& operator /=(Vec2 &lhs, const Vec2 &rhs)
 {
     lhs.x /= rhs.x; lhs.y /= rhs.y;
     return lhs;
 }
 
 
-constexpr inline Vec2 operator*(const Vec2 &lhs, float scalar)
+ACOW_CONSTEXPR_STRICT inline Vec2 operator*(const Vec2 &lhs, float scalar)
 {
     return Vec2(lhs.x * scalar, lhs.y * scalar);
 }
 
-constexpr inline Vec2 operator*(float scalar, const Vec2 &rhs)
+ACOW_CONSTEXPR_STRICT inline Vec2 operator*(float scalar, const Vec2 &rhs)
 {
     return rhs * scalar;
 }
-
 
 } // namespace math
 } // namespace acow
