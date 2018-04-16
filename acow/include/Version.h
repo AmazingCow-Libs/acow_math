f6d8849916b8c0b5094ab9e2fd7bfa88cbbab941
n2omatt <n2omatt@amazingcow.com>
2018-04-16 10:06:42 -0700

    - Add the version header.

---
+++ acow/include/Version.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#pragma once
+
+//----------------------------------------------------------------------------//
+// Version                                                                    //
+//----------------------------------------------------------------------------//
+#define ACOW_MATH_VERSION_MAJOR    "1"
+#define ACOW_MATH_VERSION_MINOR    "0"
+#define ACOW_MATH_VERSION_REVISION "0"
+
+#define ACOW_MATH_VERSION           \
+        ACOW_MATH_VERSION_MAJOR "." \
+        ACOW_MATH_VERSION_MINOR "." \
+        ACOW_MATH_VERSION_REVISION
+
